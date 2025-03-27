c5bec61d371e16d1f7adc2a7bc5b64057430e6e8
Tam <traicute459@gmail.com>
2025-03-27 13:58:23 +0700

    Change port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000'
+  origin: process.env.CLIENT_URL || 3000
 }));
 app.use(express.json());
 
